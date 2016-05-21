d8e71b6a8632b3a792f245114ab5fddd4c9d2448
Trecool <trecool@TrecooldeMacBook-Air.local>
2016-05-21 22:27:30 +0800

    lab1-3 complete

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,5 +24,108 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	var nowTask int
+	var completeTask int
+	nowTask = 0
+	completeTask = 0
+	if phase == mapPhase {
+		for {
+			str := <-mr.registerChannel
+
+			//this shows that the task on worker[n] is over
+			if str[:6] == "worker" {
+				completeTask++              //one task is over, add the complete task num
+				if completeTask == ntasks { //all the task is over, break out the loop
+					break
+				}
+
+				if nowTask != ntasks {
+					//has task to assign
+					var n int
+					var temp string
+					fmt.Sscanf(str, "%s %d", &temp, &n)
+					go mr.taskFun(n, nowTask, ntasks, nios, phase)
+					nowTask++
+				}
+			} else {
+				//we assign task to this new worker
+				mr.Lock()
+				n := len(mr.workers) - 1 //the last worker
+
+				if nowTask != ntasks {
+					go mr.taskFun(n, nowTask, ntasks, nios, phase)
+					nowTask++
+				}
+				mr.Unlock()
+			}
+		}
+	} else if phase == reducePhase {
+		//reduce do not need the filename
+		//now the worker are all in idle
+		//assign the task to workers
+		mr.Lock()
+		for i := 0; i < len(mr.workers); i++ {
+			go mr.taskFun(i, nowTask, ntasks, nios, phase)
+			nowTask++
+			if nowTask == ntasks {
+				break
+			}
+		}
+		mr.Unlock()
+		for {
+			str := <-mr.registerChannel
+
+			//this shows that the task on worker[n] is over
+			if str[:6] == "worker" {
+				completeTask++              //one task is over, add the complete task num
+				if completeTask == ntasks { //all the task is over, kill the workers
+					mr.killWorkers()
+					break
+				}
+
+				if nowTask != ntasks {
+					//has task to assign
+					var n int
+					var temp string
+					fmt.Sscanf(str, "%s %d", &temp, &n)
+					go mr.taskFun(n, nowTask, ntasks, nios, phase)
+					nowTask++
+				}
+			} else {
+				//we assign task to this new worker
+				mr.Lock()
+				n := len(mr.workers) - 1 //the last worker
+
+				if nowTask != ntasks {
+					go mr.taskFun(n, nowTask, ntasks, nios, phase)
+					nowTask++
+				}
+				mr.Unlock()
+			}
+
+		}
+	}
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+func (mr *Master) taskFun(n int, i int, ntasks int, nios int, phase jobPhase) {
+
+	args := new(DoTaskArgs)
+	args.File = mr.files[i]
+	args.JobName = mr.jobName
+	args.NumOtherPhase = nios
+	args.Phase = phase
+	args.TaskNumber = i
+
+	// lock for the mr.workers
+	mr.Lock()
+	defer mr.Unlock()
+
+	ok := call(mr.workers[n], "Worker.DoTask", args, new(struct{}))
+	if ok == false {
+		fmt.Printf("DoTask: worker %s dotask error\n", mr.workers[n])
+	}
+	str := fmt.Sprint("worker %d", n)
+	mr.registerChannel <- str
 }
